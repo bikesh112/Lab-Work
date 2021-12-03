0ce5c3999d284aa718adc67595427effcd619df7
bikesh112 <getbikeshchy@gmail.com>
2021-12-03 11:55:24 +0545

    labwork complete

--- list5.py
+++ list5.py
@@ -1,5 +1,11 @@
-# area of circle
+'''# area of circle
 a=22/7
 r=3
 area=22/7*(r**2)
-print("area of circle",area)
+print("area of circle",area)'''
+
+# add even number of first ten positive integer
+listNum=[2,4,6,8,10]
+sumB=sum(listNum)
+print(sumB)
+
