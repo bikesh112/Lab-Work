ca57177a75b1189ed8f57eabcf069b98dd0d28f6
bikesh112 <getbikeshchy@gmail.com>
2021-12-03 12:38:09 +0545

    update

--- lab3.py
+++
@@ -1,7 +0,0 @@
-# volume of cuboid
-a=int(input("enter the length:"))
-b=int(input("enter the breath:"))
-c=int(input("enter the height:"))
-volume=a*b*c
-print("the volume of cuboidis",volume)
-
--- lab6.py
+++
@@ -1,4 +0,0 @@
-# add even number of first ten positive integer
-listNum=[2,4,6,8,10]
-sumB=sum(listNum)
-print(sumB)
